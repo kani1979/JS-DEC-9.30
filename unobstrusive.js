81331c8c75640e22d356f5b577c6661369846ea5
egopikrishna <40632428+egopikrishna@users.noreply.github.com>
2020-01-29 18:14:29 +0530

    Add files via upload

---
+++ unobstrusive.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", function(){
+
+    document.getElementById("btn").addEventListener("click", function(){
+        alert("It works");
+    });
+
+    document.getElementById("btn").addEventListener("click", function(){
+        alert("It gaiin works");
+    })
+
+})
